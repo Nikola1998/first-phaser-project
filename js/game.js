8614dd0b5c1629c1f9777e5f42cc8b40d5231a60
Glorfindel <n.tapandzelic1@gmail.com>
2023-01-16 22:18:07 +0100

    setting up the basics

--- js/game.js
+++ js/game.js
@@ -1,10 +1,12 @@
-let gameScene = new Phaser.Scene("Game");
-
-const config = {
+let config = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
-  scene: gameScene,
+  scene: {
+    preload: preload,
+    create: create,
+    update: update,
+  },
   physics: {
     default: "arcade",
     arcade: {
@@ -15,3 +17,45 @@ const config = {
 };
 
 let game = new Phaser.Game(config);
+
+function preload() {
+  this.load.image("ground", "assets/ground.png");
+  this.load.image("background", "assets/background.png");
+  this.load.spritesheet("chap-idle", "assets/chap-idle.png", {
+    frameWidth: 128,
+    frameHeight: 128,
+  });
+  this.load.spritesheet("chap-run", "assets/chap-run.png", {
+    frameWidth: 128,
+    frameHeight: 128,
+  });
+  this.load.spritesheet("chap-die", "assets/chap-die.png", {
+    frameWidth: 128,
+    frameHeight: 128,
+  });
+}
+
+let ground;
+let player;
+
+function create() {
+  // WORLD
+  this.add.image(400, 300, "background").setScale(0.22);
+
+  ground = this.physics.add.staticGroup();
+
+  for (let i = 0; i < 14; i++) {
+    const x = i * 64 - 32;
+    ground.create(x, 600, "ground").setScale(0.5).refreshBody();
+  }
+  //WORLD
+
+  // PLAYER
+  player = this.physics.add
+    .sprite(100, 500, "chap-idle")
+    .setScale(0.5)
+    .refreshBody();
+  player.setCollideWorldBounds(true);
+}
+
+function update() {}
